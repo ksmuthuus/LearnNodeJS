2ed9034bcf4cd1f22fcb747ba57153aad78fb45d
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2019-12-16 15:16:35 +0530

    initial commit blogger app

---
+++ blog-app/app.js
@@ -0,0 +1,25 @@
+//jshint esversion:6
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const homeRouter = require('./routes/home')
+const aboutRouter = require('./routes/about')
+const contactRouter = require('./routes/contact')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use('/', homeRouter)
+app.use('/about', aboutRouter)
+app.use('/contact', contactRouter)
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
