e0d806a07672e29cbcf095a7d23cb94f1203f5bd
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2019-12-30 09:05:04 +0530

    add client side library

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const path = require('path')
+const socketio = require('socket.io')
+const http = require('http')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const publicDirPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirPath))
 
+io.on('connection', () => {
+  console.log('New WS connection!')
+})
+
 const port = process.env.NODE_PORT || 3000
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Server is running and listening on port ', port)
 })
