1c39500ee2cb7c09d86d6766f2114adf2d5bfd36
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2020-01-02 10:40:33 +0530

    testing for dockerization

--- blog-app/src/startups/db.js
+++ blog-app/src/startups/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect("mongodb://mongo:27017/blogDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connection to MongoDB Successful!"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
